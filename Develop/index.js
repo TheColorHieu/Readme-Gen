312d4c785448b5aeb686ff1d57b63fa1d7562239
Hieu Tran <hieuthanhtran98@yahoo.com>
2023-04-18 00:41:12 -0700

    added the packages we will be needing

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,18 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const util = require("util");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+//     generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+"What is the the tittle of the project",
+"Please enter a description",
+"Do you want a Table of contents?",
+"Installation process",
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
